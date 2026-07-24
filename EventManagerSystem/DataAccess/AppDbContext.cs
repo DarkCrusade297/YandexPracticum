04ff3d45b5c9572195fa9c148b436d8836df5a20
Платов Артур <platov-12@mail.ru>
2026-07-24 12:54:14 +0300

    Добавлены конфигурации моделей и db context

---
+++ EventManagerSystem/DataAccess/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EventManagerSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventManagerSystem.DataAccess
+{
+    internal sealed class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<EventModel> Events => Set<EventModel>();
+        public DbSet<BookingModel> Bookings => Set<BookingModel>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
